f27f6165966b2cdd0d4fea0b7655172b68e3d582
cwingert <curtis.h@me.com>
2015-04-14 09:09:40 -0700

    scorecard error handling

--- js/app/scorecard.js
+++ js/app/scorecard.js
@@ -12,64 +12,90 @@ App.Scorecard = {
     typeComm: 0,
     typeFam: 0,
     createListeners: function() {
-        $('#scorecardModalSaveButton').on('click', function() {
-            App.Scorecard.save();
+        $('#scorecardModalSaveButton').on('click', function(event) {
+            App.Scorecard.save(event);
         });
     },
-    save: function() {
+    save: function(event) {
         var name = $('#negotiationName').val();
         var type = $('#negotiationTypeSelect').val();
         var agreement = $('input[name=negotationAgreement]:checked', '#scorecardForm').val()
         var scorecardType = $('#scorecardType');
         var agreementReached = $('#agreementReached');
         var scorecardTitle = $('#scorecardTitle');
-
-        App.Scorecard.importance = $('#sliderImportant').parent().find('.tooltip-inner').html();
-        App.Scorecard.satisfaction = $('#sliderSatisfied').parent().find('.tooltip-inner').html();
-        App.Scorecard.importanceGauge.data('text', App.Scorecard.importance);
-        App.Scorecard.importanceGauge.data('percent', App.Scorecard.importance);
-        App.Scorecard.satisfactionGauge.data('text', App.Scorecard.satisfaction);
-        App.Scorecard.satisfactionGauge.data('percent', App.Scorecard.satisfaction);
-        App.Scorecard.importanceGauge.empty();
-        App.Scorecard.satisfactionGauge.empty();
-        App.Scorecard.importanceGauge.circliful();
-        App.Scorecard.satisfactionGauge.circliful();
-
-        agreementReached.html(agreement);
-        scorecardType.html(type);
-        scorecardTitle.html(name);
-
-        switch (type) {
-            case "Professional, with outside parties (vendors, customers, stakeholders, etc.)":
-                App.Scorecard.typeProOut++;
-                break;
-            case "Professional, with colleagues within your organization":
-                App.Scorecard.typeProColl++;
-                break;
-            case "Personal (such as buying a car or renting an apartment)":
-                App.Scorecard.typePersonal++;
-                break;
-            case "Community (with neighborhood groups, not-for-profits, etc.)":
-                App.Scorecard.typeComm++;
-                break;
-            case "Family (with children parents, partners, spouses, etc.)":
-                App.Scorecard.typeFam++;
-                break;
+        var valid = true;
+        if (name.trim() === '') {
+            valid = false;
+            event.preventDefault();
+            event.stopImmediatePropagation();
+            alert('Please enter a negotiation name.');
+            return;
         }
 
-        switch (agreement) {
-            case "Yes":
-                App.Scorecard.agreementYes++;
-                break;
-            case "No":
-                App.Scorecard.agreementNo++;
-                break;
-            case "Not Yet":
-                App.Scorecard.agreementNotYet++;
-                break;
+        if (type === 'Select a negotiation type below..') {
+            valid = false;
+            event.preventDefault();
+            event.stopImmediatePropagation();
+            alert('Please select a negotiation type.');
+            return;
         }
 
-        App.BestPractices.update();
+        if (agreement !== 'Yes' && agreement !== 'No' && agreement !== 'Not Yet' ) {
+            valid = false;
+            event.preventDefault();
+            event.stopImmediatePropagation();
+            alert('Please select a negotiation agreement.');
+            return;
+        }
+
+        if (valid) {
+            App.Scorecard.importance = $('#sliderImportant').parent().find('.tooltip-inner').html();
+            App.Scorecard.satisfaction = $('#sliderSatisfied').parent().find('.tooltip-inner').html();
+            App.Scorecard.importanceGauge.data('text', App.Scorecard.importance);
+            App.Scorecard.importanceGauge.data('percent', App.Scorecard.importance);
+            App.Scorecard.satisfactionGauge.data('text', App.Scorecard.satisfaction);
+            App.Scorecard.satisfactionGauge.data('percent', App.Scorecard.satisfaction);
+            App.Scorecard.importanceGauge.empty();
+            App.Scorecard.satisfactionGauge.empty();
+            App.Scorecard.importanceGauge.circliful();
+            App.Scorecard.satisfactionGauge.circliful();
+
+            agreementReached.html(agreement);
+            scorecardType.html(type);
+            scorecardTitle.html(name);
+
+            switch (type) {
+                case "Professional, with outside parties (vendors, customers, stakeholders, etc.)":
+                    App.Scorecard.typeProOut++;
+                    break;
+                case "Professional, with colleagues within your organization":
+                    App.Scorecard.typeProColl++;
+                    break;
+                case "Personal (such as buying a car or renting an apartment)":
+                    App.Scorecard.typePersonal++;
+                    break;
+                case "Community (with neighborhood groups, not-for-profits, etc.)":
+                    App.Scorecard.typeComm++;
+                    break;
+                case "Family (with children parents, partners, spouses, etc.)":
+                    App.Scorecard.typeFam++;
+                    break;
+            }
+
+            switch (agreement) {
+                case "Yes":
+                    App.Scorecard.agreementYes++;
+                    break;
+                case "No":
+                    App.Scorecard.agreementNo++;
+                    break;
+                case "Not Yet":
+                    App.Scorecard.agreementNotYet++;
+                    break;
+            }
+
+            App.BestPractices.update();
+        }
     },
     init: function() {
         var footerAlert = $('#scorecardModalFooterAlert');
