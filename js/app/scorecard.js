376baeee393c4288cd1ec2341419e652d5fb5c1f
cwingert <curtis.h@me.com>
2015-04-14 06:57:56 -0700

    slider's working

--- js/app/scorecard.js
+++ js/app/scorecard.js
@@ -9,27 +9,26 @@ App.Scorecard = {
     save: function() {
         var name = $('#negotiationName').val();
         var type = $('#negotiationTypeSelect').val();
-        var importance = $('#sliderImportant').slider('getValue');
         var agreement = $('input[name=negotationAgreement]:checked', '#scorecardForm').val()
-        var satisfaction = $('#sliderSatisfied').slider('getValue');
-        var importanceGauge = $('#importance2');
-        var satisfactionGauge = $('#satisfaction2');
+        var importance = $('#sliderImportant').parent().find('.tooltip-inner').html();
+        var satisfaction = $('#sliderSatisfied').parent().find('.tooltip-inner').html();
         var scorecardType = $('#scorecardType');
         var agreementReached = $('#agreementReached');
         var scorecardTitle = $('#scorecardTitle');
 
-        App.Scorecard.importanceGauge.empty();
-        App.Scorecard.satisfactionGauge.empty();
 
-        App.Scorecard.importanceGauge.data('text', 15);
-        App.Scorecard.importanceGauge.data('percent', 15);
-        App.Scorecard.satisfactionGauge.data('text', 25);
-        App.Scorecard.satisfactionGauge.data('percent', 25);
+
+        App.Scorecard.importanceGauge.data('text', importance);
+        App.Scorecard.importanceGauge.data('percent', importance);
+        App.Scorecard.satisfactionGauge.data('text', satisfaction);
+        App.Scorecard.satisfactionGauge.data('percent', satisfaction);
 
         agreementReached.html(agreement);
         scorecardType.html(type);
         scorecardTitle.html(name);
-
+        
+        App.Scorecard.importanceGauge.empty();
+        App.Scorecard.satisfactionGauge.empty();
         App.Scorecard.importanceGauge.circliful();
         App.Scorecard.satisfactionGauge.circliful();
     },
